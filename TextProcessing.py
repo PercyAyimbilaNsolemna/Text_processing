81ceb6986fb116b1b0d16b58629dacca624fdb57
master <pnayimbila@st.ug.edu.gh>
2023-03-31 13:52:30 +0000

    Added a getter and setter for the text attribute.

--- TextProcessing.py
+++ TextProcessing.py
@@ -7,4 +7,10 @@ class TextProcessing:
     def __str__(self):
         return "This is a text processing class"
 
-        
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self._text = text   
