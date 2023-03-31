2f6877e530f68f74e1e87874a201dcb7222acfd3
master <pnayimbila@st.ug.edu.gh>
2023-03-31 16:02:30 +0000

    Added code to test the attributes and methods in the TextProcessing class.

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
 from TextProcessing import TextProcessing 
+
+def main():
+    textProcessing = TextProcessing()
+    print(textProcessing)
+
+    textProcessing.text = "I am Percy. Percy is coming"
+    print(f"Text: {textProcessing.text}")
+
+    print(textProcessing.count_words())
+
+    print(textProcessing.count_characters())
+
+    print(textProcessing.replace("Percy", "Agnes"))
+
+    textProcessing.text = "Dennis has written codes. The codes are working properly"
+    print(textProcessing.replace_all("codes", "lines of code"))
+
+    print(textProcessing.search("codes"))
+
+    print(textProcessing.search("lines"))
+
+if __name__ == "__main__":
+    main()
